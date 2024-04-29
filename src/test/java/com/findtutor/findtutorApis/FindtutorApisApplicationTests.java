e4e513bb356db88fbfca60805d53aff7e7f0945b
kundan4ever <kundankmr919@gmail.com>
2024-04-29 11:40:50 +0530

    Initial Setup

---
+++ src/test/java/com/findtutor/findtutorApis/FindtutorApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findtutor.findtutorApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindtutorApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
